2366e96504be3fc18182fbf55b571aa23acc23e8
oliver <omar0006@student.monash.edu>
2023-10-31 10:16:07 +1100

    Finished rick write up, ready for deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import type { AppProps } from 'next/app';
 import './globals.css';
 import 'tailwindcss/tailwind.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <link rel='icon' href='o.png' />
+        <title>Oliver Markey</title>
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp;
